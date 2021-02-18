8420d468e8a4be58fad2f7a7ca4e4242f4ae63a8
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 15:45:00 +0800

    Java Practice -> MathFloorMod

---
+++ Practice/2021-02-18/MathFloorMod.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 了解数学库中floorMod函数的使用方式
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class MathFloorMod
+{
+    public static void main(String[] args)
+    {
+        int remainder = Math.floorMod(-5, 2);
+        System.out.println(remainder);
+    }
+}

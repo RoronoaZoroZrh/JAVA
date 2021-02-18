25bcd5fc89431c0e3fb4850b8150e4f585d66199
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 15:05:15 +0800

    Java Practice -> MathPow

---
+++ Practice/2021-02-18/MathPow.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 了解数学库中pow函数的使用方式
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class MathPow
+{
+    public static void main(String[] args)
+    {
+        double y = Math.pow(2, 3);
+        System.out.println(y);
+    }
+}

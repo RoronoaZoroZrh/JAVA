b0cd97c07458b1c331b040ebaca6b7a11bfff289
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 10:04:42 +0800

    Java Practice -> Division

---
+++ Practice/2021-02-18/Division.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 查看整数和浮点数被零除的结果->浮点数得到结果Infinity、整数除零抛异常
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class Division
+{
+    public static void main(String[] args)
+    {
+        System.out.println(5.0 / 0);
+        System.out.println(5 / 0);
+    }
+}

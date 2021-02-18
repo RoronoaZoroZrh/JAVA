7c621fbea224cd440dd17955b51152219188ef85
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 15:51:23 +0800

    Java Practice -> MathSqrt

--- Practice/2021-02-18/MathSqrt.java
+++ Practice/2021-02-18/MathSqrt.java
@@ -3,12 +3,15 @@
  * 日期: 2021-02-18
  * 作者: ZRH
  */
+import static java.lang.Math.*;
+
 public class MathSqrt
 {
     public static void main(String[] args)
     {
         double x = 4;
-        double y = Math.sqrt(x);
+        double y = sqrt(x);
         System.out.println(y);
+        System.out.println("The square root of \u03C0 is " + sqrt(PI));
     }
 }

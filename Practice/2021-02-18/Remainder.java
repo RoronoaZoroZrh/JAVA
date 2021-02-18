b8da23b98f4a7004e51967d4cd278bcbbaf33209
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 15:15:27 +0800

    Java Practice -> Remainder

---
+++ Practice/2021-02-18/Remainder.java
@@ -0,0 +1,15 @@
+/**
+ * 目的: 查看负数的余数->-5%2=-1 -4%2=0
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class Remainder
+{
+    public static void main(String[] args)
+    {
+        int x = -5 % 2;
+        int y = -4 % 2;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}

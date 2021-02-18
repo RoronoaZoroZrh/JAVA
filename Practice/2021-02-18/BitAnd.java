3fdfd3e1751f1b678f06715ead7d851df186bdb2
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 16:57:11 +0800

    Java Practice -> BitAnd

---
+++ Practice/2021-02-18/BitAnd.java
@@ -0,0 +1,14 @@
+/**
+ * 目的: 学习按位与运算
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class BitAnd
+{
+    public static void main(String[] args)
+    {
+        int n = 24;
+        int m = n & 0b1000;
+        System.out.println(m);
+    }
+}

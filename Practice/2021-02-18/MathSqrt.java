7e5dcb96a96de60697934d43014a77acfffe0262
RoronoaZoroZrh <137147783@qq.com>
2021-02-18 14:57:55 +0800

    Java Practice -> MathSqrt

---
+++ Practice/2021-02-18/MathSqrt.java
@@ -0,0 +1,14 @@
+/**
+ * 目的: 了解数学库中sqrt函数的使用方式
+ * 日期: 2021-02-18
+ * 作者: ZRH
+ */
+public class MathSqrt
+{
+    public static void main(String[] args)
+    {
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+    }
+}

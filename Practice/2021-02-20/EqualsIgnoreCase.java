ed5801bc889b3ffd4bbe58058a1b67762ff214ec
RoronoaZoroZrh <137147783@qq.com>
2021-02-20 14:46:32 +0800

    Java Practice -> EqualsIgnoreCase

---
+++ Practice/2021-02-20/EqualsIgnoreCase.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 学习String.equalsIgnoreCase的用法
+ * 日期: 2021-02-20
+ * 作者: ZRH
+ */
+public class EqualsIgnoreCase
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello".equals("hello"));
+        System.out.println("Hello".equalsIgnoreCase("hello"));
+    }
+}

daf5708575fd8234400fb5f410764d707107554c
RoronoaZoroZrh <137147783@qq.com>
2021-02-20 15:00:57 +0800

    Java Practice -> CompareTo

---
+++ Practice/2021-02-20/CompareTo.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 学习String.compareTo的用法
+ * 日期: 2021-02-20
+ * 作者: ZRH
+ */
+public class CompareTo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello".compareTo("Hello") == 0);
+        System.out.println("Hello".compareTo("hello") == 0);
+    }
+}

4c2b2997596452baed563aec4fa4d0ca9b583a9d
RoronoaZoroZrh <137147783@qq.com>
2021-02-20 14:40:40 +0800

    Java Practice -> Equals

---
+++ Practice/2021-02-20/Equals.java
@@ -0,0 +1,14 @@
+/**
+ * 目的: 学习String.equals的用法
+ * 日期: 2021-02-20
+ * 作者: ZRH
+ */
+public class Equals
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Hello";
+        System.out.println(greeting.equals("Hello"));
+        System.out.println("Hello".equals(greeting));
+    }
+}

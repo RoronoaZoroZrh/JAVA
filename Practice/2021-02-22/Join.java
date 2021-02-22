001ad4d63683b21e23445b97588ab9f14c292e99
RoronoaZoroZrh <137147783@qq.com>
2021-02-22 09:28:25 +0800

    Java Practice -> Join

---
+++ Practice/2021-02-22/Join.java
@@ -0,0 +1,13 @@
+/**
+ * 目的: 学习String.join的用法
+ * 日期: 2021-02-22
+ * 作者: ZRH
+ */
+public class Join
+{
+    public static void main(String[] args)
+    {
+        String s = String.join("***", "a", "b", "c");
+        System.out.println(s);
+    }
+}

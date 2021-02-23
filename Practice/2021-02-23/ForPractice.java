b6e75c990981214b8db81d81cba7c1af0061800f
RoronoaZoroZrh <137147783@qq.com>
2021-02-23 15:45:08 +0800

    Java Practice -> ForPractice

---
+++ Practice/2021-02-23/ForPractice.java
@@ -0,0 +1,16 @@
+/**
+ * 目的: 练习for语句的使用
+ * 日期: 2021-02-23
+ * 作者: ZRH
+ */
+public class ForPractice
+{
+    public static void main(String[] args)
+    {
+        for (int i = 10; i > 0; i--)
+        {
+            System.out.println("Counting down... " + i);
+        }
+        System.out.println("Blastoff!");
+    }
+}
